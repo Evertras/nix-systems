e1672d75374ae64fcba1ccf2f6700c5037f422a3
Brandon Fulljames <bfullj@gmail.com>
2024-01-01 15:05:09 +0900

    Move to subdir

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     nixosConfigurations = {
       nixbox = lib.nixosSystem {
         system = "linux-x86_64";
-        modules = [ ./configuration.nix ];
+        modules = [ ./systems/2024-win-vm-sandbox/configuration.nix ];
       };
     };
   };
