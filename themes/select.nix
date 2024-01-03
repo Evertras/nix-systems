bc5b805e72471e947520af2c31f6d7b866279af5
Brandon Fulljames <bfullj@gmail.com>
2024-01-03 21:46:02 +0900

    Move comment for clarity

--- themes/select.nix
+++ themes/select.nix
@@ -2,9 +2,6 @@
 with lib;
 let cfg = config.evertras.themes;
 in {
-  # Defines a bunch of constants for use in other
-  # modules to create a consistent theme that can be
-  # easily switched
   options.evertras.themes = {
     selected = mkOption {
       type = types.str;
--- themes/themes.nix
+++ themes/themes.nix
@@ -1,10 +1,13 @@
+# Defines a bunch of constants for use in other
+# modules to create a consistent theme that can be
+# easily switched
 {
   mint = {
     # https://coolors.co/ef6f6c-2e394d-dcf9eb-59c9a5-7a907c
     colors = {
       primary = "#59C9A5";
       highlight = "#A7F1CD";
-      background = "#2e394d";
+      background = "#2E394D";
       text = "#DCF9EB";
       urgent = "#EF6F6C";
     };
