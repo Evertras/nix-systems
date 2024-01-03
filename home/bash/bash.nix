053317318a1f8ecf6f387c6885eafb3d5269a7f0
Brandon Fulljames <bfullj@gmail.com>
2024-01-03 13:25:14 +0900

    Moving home modules out

---
+++ home/bash/bash.nix
@@ -0,0 +1,117 @@
+{ config, pkgs, ... }:
+
+{
+  programs = {
+    bash = {
+      enable = true;
+      shellAliases = {
+        grep = "grep --color=auto";
+        k = "kubectl";
+        ls = "ls --color";
+        mux = "tmuxinator";
+        nr = "npm run";
+        reloadbash = "source ~/.bashrc";
+        vim = "nvim";
+      };
+
+      # bashrcExtra for all shells, initExtra for interactive only
+      initExtra = ''
+        # Don't show control characters
+        stty -echoctl
+
+        # Make GPG signing happen in the correct terminal
+        export GPG_TTY="$(tty)"
+
+        # Simple Makefile completion
+        complete -W "\`grep -oE '^[a-zA-Z0-9_-]+:([^=]|$)' Makefile | sed 's/[^a-zA-Z0-9_-]*$//'\`" make
+
+        # Usage: up [n]
+        #
+        # Example: 'up 3' goes up 3 directories
+        up() {
+          local d=""
+          limit=$1
+          for((i=1 ; i <= limit ; i++))
+            do
+              d=$d/..
+            done
+
+          d=$(echo $d | sed 's/^\///')
+          if [ -z "$d" ]; then
+            d=..
+          fi
+
+          cd $d
+        }
+
+        function git-merged() {
+          branch=$(git rev-parse --abbrev-ref HEAD)
+          git checkout main
+          git pull
+          git branch -d "''${branch}"
+        }
+
+        function aws-connect() {
+          aws ssm start-session --target "''${1}"
+        }
+
+        function aws-ec2-list() {
+          aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" |
+            jq -r '.Reservations | .[] | .Instances | .[] | { Id: .InstanceId, Name: (.Tags[] | select(.Key == "Name") | .Value) } | [.Name, .Id] | @tsv' |
+            sort |
+            column -t
+        }
+
+        function kitty-reload() {
+          kill -SIGUSR1 $(pgrep kitty)
+        }
+
+        function kitty-theme() {
+          kitten theme --reload-in=all --config-file-name theme.conf
+          sleep 1 && kitty-reload
+        }
+
+        function fonts() {
+          fc-list : family | awk -F, '{print $1}' | grep Nerd | grep -E 'Mono$' | sort -u
+        }
+
+        function show-color() {
+            perl -e 'foreach $a(@ARGV){print "\e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m "};print "\n"' "$@"
+        }
+
+        # For funsies, only adjusts kitty theme for now
+        function retheme() {
+          searchterm="$@"
+          if [ -z "''${searchterm}" ]; then
+            searchterm=mountain
+          fi
+
+          if ! type schemer2 &> /dev/null; then
+            mkdir -p ~/bin
+            GOBIN=~/bin/schemer2 go install github.com/thefryscorer/schemer2@latest
+          fi
+
+          echo "Retheming to ''${searchterm}"
+          styli.sh -s "''${searchterm}"
+          colors=$(schemer2 -format img::colors -in ~/.cache/styli.sh/wallpaper.jpg)
+          IFS=$'\n'
+          for color in ''${colors}; do
+            # Hijacked from show-color above
+            perl -e 'foreach $a(@ARGV){print "\e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m"};' "''${color:1}"
+          done
+          schemer2 -format img::kitty -in ~/.cache/styli.sh/wallpaper.jpg > ~/.config/kitty/theme.conf
+          kill -SIGUSR1 $(pgrep kitty)
+        }
+
+        # Machine-specific bash stuff should go in this directory
+        if [[ -d ~/.bashrc.d ]]; then
+          for src in ~/.bashrc.d/*; do
+            if [[ -f ''${src} ]]; then
+              source ''${src}
+            fi
+          done
+        fi
+      '';
+    };
+  };
+}
---
+++ home/kitty/kitty.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  options = {
+  };
+
+  config = {
+    programs = {
+      kitty = {
+        enable = true;
+
+        font = {
+          name = "Hasklug Nerd Font Mono";
+          size = 14;
+          package = pkgs.nerdfonts;
+        };
+
+        extraConfig = ''
+          # I like changing the theme a lot on a whim, this
+          # file is created/modified by the bash function kitty-theme
+          include theme.conf
+
+          # Overriding things in a pinch, such as demo font size switch
+          include override.conf
+        '';
+
+        settings = {
+          background_opacity = "0.8";
+          # No blinking
+          cursor_blink_interval = 0;
+          cursor_shape = "block";
+          enable_audio_bell = "no";
+          # Don't change to bar when typing
+          shell_integration = "no-cursor";
+          strip_trailing_spaces = "smart";
+        };
+      };
+    };
+  };
+}
