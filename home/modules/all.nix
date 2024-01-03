e93aa6181dca492a24d491810adddb2b2791768b
Brandon Fulljames <bfullj@gmail.com>
2024-01-03 21:49:51 +0900

    Switch to default filename for nicer imports in home

--- home/modules/all.nix
+++ home/modules/all.nix
@@ -2,14 +2,6 @@
 
 {
   # These are all controlled by enable flags
-  imports = [
-    ./bash/bash.nix
-    ./core/core.nix
-    ./editorconfig/editorconfig.nix
-    ./i3/i3.nix
-    ./kitty/kitty.nix
-    ./starship/starship.nix
-    ./theme/theme.nix
-    ./tmux/tmux.nix
-  ];
+  imports =
+    [ ./bash ./core ./editorconfig ./i3 ./kitty ./starship ./theme ./tmux ];
 }
