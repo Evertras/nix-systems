f125814075f495a5e518316a110ac9b8b43b4cdf
Brandon Fulljames <bfullj@gmail.com>
2024-01-03 11:46:07 +0900

    Add home manager channel

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 .PHONY: rebuild
 rebuild:
 	@./scripts/ensure-passwords.sh
+	@./scripts/ensure-channel.sh
 	sudo nixos-rebuild switch --flake .
 	@sudo -u evertras ./scripts/ensure-rcs.sh
 
---
+++ scripts/ensure-channel.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+if sudo nix-channel --list | grep home-manager &>/dev/null; then
+  # Don't need to do anything
+  exit 0
+fi
+
+# Get matching version with NixOS - may not work with unstable
+raw=$(sudo nix-channel --list | grep nixos)
+url="https://github.com/nix-community/home-manager/archive/release-${raw#*nixos-}.tar.gz" 
+sudo nix-channel --add "${url}" home-manager
+sudo nix-channel --update
