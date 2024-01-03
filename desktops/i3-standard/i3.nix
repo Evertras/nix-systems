b1af6fc734f334537d707b7a63528e42167d60e4
Brandon Fulljames <bfullj@gmail.com>
2024-01-03 11:19:33 +0900

    First configuration

--- desktops/i3-standard/i3.nix
+++ desktops/i3-standard/i3.nix
@@ -1,43 +1,57 @@
+# Desktop environment
+# https://nixos.wiki/wiki/I3
 { config, lib, pkgs, ... }:
-{
-  # Desktop environment
-  # https://nixos.wiki/wiki/I3
-  services.xserver = {
-    enable = true;
-    layout = "jp";
+with lib;
+let
+  cfg = config.evertras.desktop.i3;
+in {
+  options.evertras.desktop.i3 = {
+    enable = mkEnableOption "i3 desktop";
 
-    displayManager = {
-      defaultSession = "none+i3";
-
-      # Sleep seems to be required for avoiding some init race
-      # not great but works for now.  Note that if the resolution
-      # doesn't change, check video memory in VM settings.
-      sessionCommands = ''
-        xrandr --output Virtual1 --mode 2560x1440
-        picom -f &
-        (sleep 1s && setxkbmap -layout jp && styli.sh -s mountain) &
-      '';
-
-      # Explicitly enable lightDM in case we log back out,
-      # just to remind ourselves which thing we're using...
-      lightdm = {
-        enable = true;
-      };
+    kbLayout = mkOption {
+      type = types.str;
+      default = "us";
     };
+  };
 
-    windowManager.i3 = {
+  config = mkIf cfg.enable {
+    services.xserver = {
       enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3lock
-        picom-next
-        # TODO: look at i3blocks?
-      ];
-    };
+      layout = cfg.kbLayout;
 
-    # Disable capslock, trying to remap it to ctrl
-    # seems to do some weird things
-    xkb.options = "caps:none";
+      displayManager = {
+        defaultSession = "none+i3";
+
+        # Sleep seems to be required for avoiding some init race
+        # not great but works for now.  Note that if the resolution
+        # doesn't change, check video memory in VM settings.
+        sessionCommands = ''
+          xrandr --output Virtual1 --mode 2560x1440
+          picom -f &
+          (sleep 1s && setxkbmap -layout ${cfg.kbLayout} && styli.sh -s mountain) &
+        '';
+
+        # Explicitly enable lightDM in case we log back out,
+        # just to remind ourselves which thing we're using...
+        lightdm = {
+          enable = true;
+        };
+      };
+
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu
+          i3status
+          i3lock
+          picom-next
+          # TODO: look at i3blocks?
+        ];
+      };
+
+      # Disable capslock, trying to remap it to ctrl
+      # seems to do some weird things
+      xkb.options = "caps:none";
+    };
   };
 }
--- systems/vm-nixbox/configuration.nix
+++ systems/vm-nixbox/configuration.nix
@@ -9,6 +9,10 @@
     ./hardware-configuration.nix
   ];
 
+  evertras.desktop.i3 = {
+    enable = true;
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
