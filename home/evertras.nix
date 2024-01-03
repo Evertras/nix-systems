def7ad1e4446a597b9c1ee48f7e7f3f6e41723bf
Brandon Fulljames <bfullj@gmail.com>
2024-01-03 12:40:04 +0900

    Migrating more functions over

--- home/evertras.nix
+++ home/evertras.nix
@@ -49,8 +49,76 @@
       shellAliases = {
         ls = "ls --color";
         grep = "grep --color=auto";
+        mux = "tmuxinator";
+        reloadbash = "source ~/.bashrc";
         vim = "nvim";
       };
+      sessionVariables = {
+        VISUAL = "nvim";
+      };
+      # bashrcExtra for all shells, initExtra for interactive only
+      initExtra = ''
+        # Simple Makefile completion
+        complete -W "\`grep -oE '^[a-zA-Z0-9_-]+:([^=]|$)' Makefile | sed 's/[^a-zA-Z0-9_-]*$//'\`" make
+
+        function git-merged() {
+          branch=$(git rev-parse --abbrev-ref HEAD)
+          git checkout main
+          git pull
+          git branch -d "''${branch}"
+        }
+
+        function aws-connect() {
+          aws ssm start-session --target "''${1}"
+        }
+
+        function aws-ec2-list() {
+          aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" |
+            jq -r '.Reservations | .[] | .Instances | .[] | { Id: .InstanceId, Name: (.Tags[] | select(.Key == "Name") | .Value) } | [.Name, .Id] | @tsv' |
+            sort |
+            column -t
+        }
+
+        function kitty-theme() {
+          kitten theme --reload-in=all --config-file-name theme.conf
+          kill -SIGUSR1 $(pgrep kitty)
+        }
+
+        function kitty-reload() {
+          kill -SIGUSR1 $(pgrep kitty)
+        }
+
+        function fonts() {
+          fc-list : family | awk -F, '{print $1}' | grep Nerd | grep -E 'Mono$' | sort -u
+        }
+
+        function show-color() {
+            perl -e 'foreach $a(@ARGV){print "\e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m "};print "\n"' "$@"
+        }
+
+        function retheme() {
+          searchterm="$@"
+          if [ -z "''${searchterm}" ]; then
+            searchterm=mountain
+          fi
+
+          if ! type schemer2 &> /dev/null; then
+            mkdir -p ~/bin
+            GOBIN=~/bin/schemer2 go install github.com/thefryscorer/schemer2@latest
+          fi
+
+          echo "Retheming to ''${searchterm}"
+          styli.sh -s "''${searchterm}"
+          colors=$(schemer2 -format img::colors -in ~/.cache/styli.sh/wallpaper.jpg)
+          IFS=$'\n'
+          for color in ''${colors}; do
+            # Hijacked from show-color above
+            perl -e 'foreach $a(@ARGV){print "\e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m"};' "''${color:1}"
+          done
+          schemer2 -format img::kitty -in ~/.cache/styli.sh/wallpaper.jpg > ~/.config/kitty/theme.conf
+          kill -SIGUSR1 $(pgrep kitty)
+        }
+      '';
     };
 
     starship = {
