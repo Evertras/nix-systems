98c093ababb3fed0b0068ffbc20a658e05a2d0ec
Brandon Fulljames <bfullj@gmail.com>
2024-01-03 12:51:29 +0900

    Add note to self for future for starship config tweaks

--- home/evertras.nix
+++ home/evertras.nix
@@ -157,6 +157,14 @@
 
     starship = {
       enable = true;
+
+      settings = {
+        # TODO: only do this in WSL
+        # This fixes a weird thing where it says "Systemd"
+        # at the start of the prompt in WSL.
+        # https://www.reddit.com/r/fishshell/comments/yhoi28/im_using_starship_prompt_in_wsl_and_it_keep/
+        #container.disabled = true;
+      };
     };
   };
 }
