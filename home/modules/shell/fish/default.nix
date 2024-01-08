7b6edbe3707c206b43d1ff21b7a16fef910f71d6
Brandon Fulljames <bfullj@gmail.com>
2024-01-08 18:45:56 +0900

    Add generated fish theme

--- home/modules/shell/fish/default.nix
+++ home/modules/shell/fish/default.nix
@@ -23,10 +23,41 @@ in {
       };
 
       shellInit = let
-        colorPrimary = string.removePrefix "#" theme.colors.primary;
-        colorText = string.removePrefix "#" theme.colors.text;
+        rmp = strings.removePrefix "#";
+        colorPrimary = rmp theme.colors.primary;
+        colorText = rmp theme.colors.text;
+        colorUrgent = rmp theme.colors.urgent;
       in ''
-        set -U fish_greeting Everfish
+        set -g fish_greeting Everfish
+
+        # Theme
+        # TODO: Move this into theme file
+        set -g fish_color_normal c6d0f5
+        set -g fish_color_command 8caaee
+        set -g fish_color_param eebebe
+        set -g fish_color_keyword e78284
+        set -g fish_color_quote a6d189
+        set -g fish_color_redirection f4b8e4
+        set -g fish_color_end ef9f76
+        set -g fish_color_comment 838ba7
+        set -g fish_color_error ${colorUrgent}
+        set -g fish_color_gray 737994
+        set -g fish_color_selection --background=414559
+        set -g fish_color_search_match --background=414559
+        set -g fish_color_option a6d189
+        set -g fish_color_operator f4b8e4
+        set -g fish_color_escape ea999c
+        set -g fish_color_autosuggestion 737994
+        set -g fish_color_cancel e78284
+        set -g fish_color_cwd e5c890
+        set -g fish_color_user 81c8be
+        set -g fish_color_host 8caaee
+        set -g fish_color_host_remote a6d189
+        set -g fish_color_status e78284
+        set -g fish_pager_color_progress 737994
+        set -g fish_pager_color_prefix f4b8e4
+        set -g fish_pager_color_completion c6d0f5
+        set -g fish_pager_color_description 737994
       '';
 
       # TODO: Figure out how to get this working, it installs but isn't found on theme change
@@ -44,7 +75,7 @@ in {
       */
 
       /* Scratchpad for future reference:
-         fish --private (basically fish incognito, no history save, useful for dealing with credentials)
+         fish --private ()
       */
     };
   };
