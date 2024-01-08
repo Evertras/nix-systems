d56a831b4b57f11ac317af11f34480d9dc06823a
Brandon Fulljames <bfullj@gmail.com>
2024-01-08 18:56:25 +0900

    Add monitor sleep config and some todos

--- home/users/work.nix
+++ home/users/work.nix
@@ -29,17 +29,29 @@ in {
       i3 = {
         monitorNetworkInterface = "eno1";
 
+        # TODO: Fix 'Mod4'
         keybindOverrides = { "Mod4+space" = "exec nixGL kitty"; };
 
         startupPreCommands = [
+          # TODO: Could be fun making this a module?
           "xrandr --output ${displayCenter} --auto --output ${displayRight} --right-of ${displayCenter} --auto --rotate left --output ${displayLeft} --left-of ${displayCenter} --auto"
         ];
 
         startupPostCommands = [
           # Prebuilt picom that works on this machine since it's not nixOS
           "/home/brandon-fulljames/bin/picom &"
+
+          # Screensaver, eventually... maybe?  Keeping for now for quick reference/inspiration
+          #"xset s 3600"
+
+          # Monitor sleep settings (TODO: pull this into desktop config)
+          # Units in seconds
+          # man xset -> "The first value given is for the ‘standby' mode, the second is for the ‘suspend' mode, and the third is for the ‘off' mode."
+          # So basically, standby after 10 minutes, then suspend after an hour, then turn off after 3 hours
+          "xset dpms 600 3600 10800"
         ];
 
+        # Don't want status in right monitor since it's vertical
         bars = [
           {
             id = "main";
