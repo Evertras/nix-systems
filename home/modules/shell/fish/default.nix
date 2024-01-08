8ce41bd417a2c15a861c384525bdfff8d0cf88d5
Brandon Fulljames <bfullj@gmail.com>
2024-01-08 09:05:45 +0900

    Plugin reference WIP

--- home/modules/shell/fish/default.nix
+++ home/modules/shell/fish/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 with lib;
 let theme = config.evertras.themes.selected;
 in {
@@ -28,6 +28,20 @@ in {
       in ''
         set -U fish_greeting Everfish
       '';
+
+      # TODO: Figure out how to get this working, it installs but isn't found on theme change
+      /* plugins = [
+           {
+             name = "z";
+             src = pkgs.fetchFromGitHub {
+               owner = "catppuccin";
+               repo = "fish";
+               rev = "0ce27b518e8ead555dec34dd8be3df5bd75cff8e";
+               sha256 = "sha256-Dc/zdxfzAUM5NX8PxzfljRbYvO9f9syuLO8yBr+R3qg=";
+             };
+           }
+         ];
+      */
 
       /* Scratchpad for future reference:
          fish --private (basically fish incognito, no history save, useful for dealing with credentials)
