6461f6782697f6ba4c44f44e89c76c42995595d9
Brandon Fulljames <bfullj@gmail.com>
2024-01-09 22:54:22 +0900

    Enable autologin for dwm

--- system/modules/desktop/dwm/default.nix
+++ system/modules/desktop/dwm/default.nix
@@ -1,18 +1,16 @@
-{config, lib, pkgs,...}:
+{ config, lib, pkgs, ... }:
 with lib;
 let cfg = config.evertras.desktop.dwm;
-in
-{
-  options.evertras.desktop.dwm = {
-    enable = mkEnableOption "dwm";
-  };
+in {
+  options.evertras.desktop.dwm = { enable = mkEnableOption "dwm"; };
 
   config = mkIf cfg.enable {
     services.xserver = {
-      displayManager.defaultSession = "dwm";
+      displayManager.defaultSession = "none+dwm";
 
-      windowManager.dwm.package = pkgs.dwm.overrideAttrs {
-        src = ./src;
+      windowManager.dwm = {
+        enable = true;
+        package = pkgs.dwm.overrideAttrs { src = ./src; };
       };
     };
   };
