fde0429bfc8912d4ab0b31f5120ee975bb09da55
Brandon Fulljames <bfullj@gmail.com>
2024-01-10 23:53:05 +0900

    Add custom dwm to home

--- home/users/work.nix
+++ home/users/work.nix
@@ -88,26 +88,15 @@ in {
 
   home = {
     # Other local things
-    packages = with pkgs;
-      [
-        # Even without sound, allows remote control
-        spotify-tui
-      ];
+    packages = with pkgs; [
+      # Even without sound, allows remote control
+      spotify-tui
 
-    file = {
-      ".asdfrc".text = "legacy_version_file = yes";
+      # User-accessible version of custom dwm
+      (import ../../shared/dwm { inherit lib pkgs theme; })
+    ];
 
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
-      # ".screenrc".source = dotfiles/screenrc;
-
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
-    };
+    file = { ".asdfrc".text = "legacy_version_file = yes"; };
 
     # Don't change this, this is the initial install version
     stateVersion = "23.05"; # Please read the comment before changing.
