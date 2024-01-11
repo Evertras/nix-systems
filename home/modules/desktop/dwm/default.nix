6fb00d57164166e6185fc7d58b78787d5281464b
Brandon Fulljames <bfullj@gmail.com>
2024-01-11 11:10:01 +0900

    Add dwm install script for sudo sadness

--- home/modules/desktop/dwm/default.nix
+++ home/modules/desktop/dwm/default.nix
@@ -7,5 +7,27 @@ let
 in {
   options.evertras.home.desktop.dwm = { enable = mkEnableOption "dwm"; };
 
-  config = mkIf cfg.enable { home.packages = [ customDwm ]; };
+  config = mkIf cfg.enable {
+    home.packages = [ customDwm ];
+
+    home.file = {
+      ".evertras/systemfiles/dwm.desktop" = {
+        text = ''
+          [Desktop Entry]
+          Name=dwm
+          Comment=dynamic window manager
+          Exec=${customDwm}/bin/dwm
+          Type=XSession
+          DesktopNames=dwm
+        '';
+      };
+    };
+
+    # This unfortunately seems necessary if we're not using NixOS...
+    evertras.home.shell.funcs = {
+      "install-dwm-without-nixos".body = ''
+        sudo ln -s ~/.evertras/systemfiles/dwm.desktop /usr/share/xsessions/dwm.desktop
+      '';
+    };
+  };
 }
