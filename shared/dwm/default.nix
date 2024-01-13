8625077d8d16a759bbde5cc8604148bd04c27d15
Brandon Fulljames <bfullj@gmail.com>
2024-01-13 10:09:58 +0900

    Sort options

--- shared/dwm/default.nix
+++ shared/dwm/default.nix
@@ -2,14 +2,14 @@
 let
   patches = (import ./patches.nix) { lib = lib; };
   basePatch = patches.mkBasePatch {
-    terminal = opts.terminal;
-    lock = opts.lock;
+    colorBackground = theme.colors.background;
     colorPrimary = theme.colors.primary;
     colorText = theme.colors.text;
-    colorBackground = theme.colors.background;
     fontName = theme.fonts.main.name;
     fontSize = opts.fontSize;
     gappx = opts.gappx;
+    lock = opts.lock;
+    terminal = opts.terminal;
   };
   patchList = [ basePatch ];
 in pkgs.dwm.overrideAttrs (self: super: {
