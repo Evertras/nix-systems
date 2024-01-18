97e3ea5389e147c4bcd3ef7918449c789368f2e4
Brandon Fulljames <bfullj@gmail.com>
2024-01-19 08:58:43 +0900

    Better comment

--- home/modules/shell/default.nix
+++ home/modules/shell/default.nix
@@ -42,7 +42,7 @@ in {
       packages = with pkgs; [
         # General terminal tools
         asciigraph # graphing stuff in terminal
-        bat # cat with syntax highlighting
+        bat # cat with syntax highlighting / paging
         btop # fancy top
         dig # DNS lookup
         entr # run commands on file changes
