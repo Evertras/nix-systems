5659f907a8e427e36ac06ecd63cc451a36f2967c
Brandon Fulljames <bfullj@gmail.com>
2024-01-21 13:09:44 +0900

    Remove extra func definition

--- home/modules/shell/default.nix
+++ home/modules/shell/default.nix
@@ -141,8 +141,6 @@ in {
           perl -e 'foreach $a(@ARGV){print "\e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m "};print "\n"' "$@"
         '';
 
-        cycle-wallpaper.body = "styli.sh -s '${theme.inspiration}' -b bg-fill";
-
         git-merged.body = ''
           branch=$(git rev-parse --abbrev-ref HEAD)
           git checkout main
