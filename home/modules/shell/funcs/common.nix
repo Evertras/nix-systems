2d5092da1179e93e289384542765f437c2ba289a
Brandon Fulljames <bfullj@gmail.com>
2024-01-25 14:49:22 +0900

    Try fancier change-all

--- home/modules/shell/funcs/common.nix
+++ home/modules/shell/funcs/common.nix
@@ -14,5 +14,43 @@
 
       while sleep 1; do ls "$watchdir"/*.go | entr -c -d go test -race "$watchdir"; done
     '';
+
+    change-all.body = ''
+      if [ $# -ne 4 ]; then
+        echo "USAGE: change-all <dir> <file-filter> <old-regex> <replacement>"
+        echo ""
+        echo "       Changes all matches of <old-regex> to <new> in the"
+        echo "       <dir> directory and all subdirectories for all files"
+        echo "       that match the file-type filter.  Uses sed."
+        echo ""
+        echo "   ex: change-all '*.go' 'mypkg\.Thing' 'mypkg.BetterName'"
+        exit 1
+      fi
+
+      dir=$1
+      filefilter=$2
+      oldregex=$3
+      replacement=$4
+
+      echo "Looking in all files with filter '$filefilter'"
+
+      # Show all matches for thorough check with fzf for scrolling
+      matches=$(find "$dir" -type f -name "$filefilter" -exec grep -nE "$oldregex" {} +)
+      echo "$matches" | fzf --no-sort
+
+      # Show affected files with counts for quick final check when deciding
+      find "$dir" -type f -name "$filefilter" -exec grep -cE "$oldregex" {} + | awk -F: '$2 != 0 { print $2 " " $1 }' | col | sort -n
+
+      echo "Replacing any regex pattern match of '$oldregex' with '$replacement' in $dir"
+      read -p "Found $(wc -l <<< $matches) matches.  Continue? [y/n] " -n 1 -r
+      echo ""
+
+      if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo "Don't do it"
+        exit 1
+      fi
+
+      find "$dir" -type f -name "$filefilter" -exec sed -i "s/$oldregex/$replacement/g" {} \;
+    '';
   };
 }
