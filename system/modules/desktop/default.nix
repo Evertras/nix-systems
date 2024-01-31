2fb06472fb4bfe0dce9940b643c2a6f5279b832a
Brandon Fulljames <bfullj@gmail.com>
2024-01-31 21:06:26 +0900

    Remove i3 from system modules

--- system/modules/desktop/default.nix
+++ system/modules/desktop/default.nix
@@ -2,7 +2,7 @@
 with lib;
 let cfg = config.evertras.desktop;
 in {
-  imports = [ ./core ./i3 ../../../shared/themes/select.nix ];
+  imports = [ ./core ../../../shared/themes/select.nix ];
 
   options.evertras.desktop = { enable = mkEnableOption "desktop"; };
 
