95f68e11ea84048e83314cc47825fe1e355e126b
Brandon Fulljames <bfullj@gmail.com>
2024-02-02 12:34:27 +0900

    Add everlib func to get all files

--- shared/everlib/default.nix
+++ shared/everlib/default.nix
@@ -4,10 +4,19 @@ let concatPaths = base: subdir: base + ("/" + subdir);
 in {
   allSubdirs = path:
     let
-      fileset = builtins.readDir path;
-      dirset = filterAttrs (_: s: s == "directory") fileset;
+      # { "fileordirname" = "regular|directory" ... }
+      readset = builtins.readDir path;
+      dirset = filterAttrs (_: s: s == "directory") readset;
       dirs = map (concatPaths path) (builtins.attrNames dirset);
     in dirs;
+
+  allFiles = path:
+    let
+      # { "fileordirname" = "regular|directory" ... }
+      readset = builtins.readDir path;
+      fileset = filterAttrs (_: s: s == "regular") readset;
+      files = map (concatPaths path) (builtins.attrNames fileset);
+    in files;
 
   existsOr = a: b: if a == null then b else a;
 }
