9da78c9d6dbdbfd5e76a786aa1d96830b589e952
Brandon Fulljames <bfullj@gmail.com>
2024-02-02 16:03:53 +0900

    Fix var shadowing weirdness

--- shared/everlib/default.nix
+++ shared/everlib/default.nix
@@ -1,22 +1,22 @@
 { lib }:
 with lib; {
-  allSubdirs = path:
+  allSubdirs = rootPath:
     let
       # { "fileordirname" = "regular|directory" ... }
-      readset = builtins.readDir path;
+      readset = builtins.readDir rootPath;
       dirset = filterAttrs (_: type: type == "directory") readset;
-      dirs = map (path.append path) (builtins.attrNames dirset);
+      dirs = map (path.append rootPath) (builtins.attrNames dirset);
     in dirs;
 
-  allNixFiles = path:
+  allNixFiles = rootPath:
     let
       # { "fileordirname" = "regular|directory" ... }
-      readset = builtins.readDir path;
+      readset = builtins.readDir rootPath;
       isNixFile = strings.hasSuffix ".nix";
       fileset =
         filterAttrs (filename: type: (isNixFile filename) && type == "regular")
         readset;
-      files = map (path.append path) (builtins.attrNames fileset);
+      files = map (path.append rootPath) (builtins.attrNames fileset);
     in files;
 
   existsOr = a: b: if a == null then b else a;
