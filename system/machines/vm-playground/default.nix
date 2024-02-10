24849d1d3a014157be4c943b57969c4ba5d5c137
Brandon Fulljames <bfullj@gmail.com>
2024-02-10 09:10:54 +0900

    Shortcuts for testing the simple homemanager setup

--- system/machines/vm-playground/default.nix
+++ system/machines/vm-playground/default.nix
@@ -1,7 +1,17 @@
 # This is a VM playground to try using NixOS as a server
 # configuration tool.
 
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let
+  bootstrap = pkgs.writeShellApplication {
+    name = "bootstrap";
+    text = ''
+      mkdir -p ~/.local/state/nix/profiles
+      git clone https://github.com/Evertras/simple-homemanager
+    '';
+  };
+
+in {
   imports = [ ../../modules ./base.nix ];
 
   # https://mynixos.com/nixpkgs/options/virtualisation.docker
@@ -15,5 +25,13 @@
   };
 
   evertras.system.services.nomad.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    bootstrap
+    git
+    home-manager
+    neovim
+    gnumake
+  ];
 }
 
