533b8957ac331f84ba12cb117449d77ce8c5fe35
Brandon Fulljames <bfullj@gmail.com>
2024-02-12 21:43:55 +0900

    Explanation

--- home/modules/shell/env/default.nix
+++ home/modules/shell/env/default.nix
@@ -3,7 +3,7 @@ with lib; {
   # To be applied to individual shell rcs in ../shells/
   options.evertras.home.shell.env = {
     vars = mkOption {
-      description = "";
+      description = "Environment variables to apply to all shells";
       type = with types; attrsOf str;
       default = { };
     };
