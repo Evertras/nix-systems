73b5a83d8bbf9732faafba974e978f72e6b49da5
Brandon Fulljames <bfullj@gmail.com>
2024-02-15 23:43:39 +0900

    Add helper to install catppuccin for now

--- home/modules/desktop/inputs/default.nix
+++ home/modules/desktop/inputs/default.nix
@@ -3,6 +3,9 @@
     i18n.inputMethod = {
       enabled = "fcitx5";
 
+      # At some point try to figure out settings here.
+      # https://mynixos.com/nixpkgs/options/i18n.inputMethod.fcitx5.settings
+      # A first try got an error about that field not existing.
       fcitx5 = {
         addons = with pkgs; [
           fcitx5-gtk
@@ -11,6 +14,17 @@
           fcitx5-nord
         ];
       };
+    };
+
+    evertras.home.shell.funcs = {
+      # Some day it would be nice to make this a package,
+      # or use the NUR, but we're already doing some
+      # custom config anyway.
+      "fcitx5-install-catppuccin".body = ''
+        git clone https://github.com/catppuccin/fcitx5.git /tmp/fcitx5-catppuccin
+        mkdir -p ~/.local/share/fcitx5/themes/
+        cp -r /tmp/fcitx5-catppuccin/src/* ~/.local/share/fcitx5/themes
+      '';
     };
   };
 }
