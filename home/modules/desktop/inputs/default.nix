376242333a429919c009bff58898d0370de1222f
Brandon Fulljames <bfullj@gmail.com>
2024-02-15 23:07:24 +0900

    Add material colors

--- home/modules/desktop/inputs/default.nix
+++ home/modules/desktop/inputs/default.nix
@@ -2,7 +2,12 @@
   config = {
     i18n.inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [ fcitx5-gtk fcitx5-configtool fcitx5-mozc ];
+      fcitx5.addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-configtool
+        fcitx5-mozc
+        fcitx5-material-color
+      ];
     };
   };
 }
