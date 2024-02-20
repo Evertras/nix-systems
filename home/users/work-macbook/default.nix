1ee3f06f5599713a15f1760487b069f112be9fdd
Brandon Fulljames <bfullj@gmail.com>
2024-02-20 13:38:47 +0900

    Add work macbook

---
+++ home/users/work-macbook/default.nix
@@ -0,0 +1,4 @@
+{
+  system = "x86_64-darwin";
+  module = ./home.nix;
+}
---
+++ home/users/work-macbook/home.nix
@@ -0,0 +1,42 @@
+{ lib, nerdfonts, pkgs, ... }:
+
+let
+  themes = import ../../../shared/themes/themes.nix { inherit pkgs lib; };
+  theme = themes.mkCatppuccin { color = "Green"; };
+in {
+  imports = [ ../modules ../../shared/themes/select.nix ];
+
+  evertras.home = {
+    core = {
+      username = "brandon.fulljames";
+      homeDirectory = "/Users/brandon.fulljames";
+      usingNixOS = false;
+    };
+
+    shell = {
+      core = { inherit gpgKey; };
+
+      git.userEmail = "brandon.fulljames@woven-planet.global";
+    };
+
+    desktop = {
+      enable = false;
+
+      terminals = {
+        alacritty.enable = true;
+
+        kitty.enable = true;
+      };
+    };
+  };
+
+  home = {
+    # Other local things
+    packages = with pkgs; [ ];
+
+    file = { ".asdfrc".text = "legacy_version_file = yes"; };
+
+    # Don't change this, this is the initial install version
+    stateVersion = "23.05"; # Please read the comment before changing.
+  };
+}
