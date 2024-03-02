b415e879a238331c630fd6a7ddbf1d8559b11c1f
Brandon Fulljames <bfullj@gmail.com>
2024-03-02 15:33:44 +0900

    Switch to teal for nixtop

--- home/users/evertras-nixtop/home.nix
+++ home/users/evertras-nixtop/home.nix
@@ -1,7 +1,7 @@
 { lib, nerdfonts, pkgs, ... }:
 let
   themes = import ../../../shared/themes/themes.nix { inherit pkgs lib; };
-  theme = themes.mkCatppuccin { color = "Peach"; };
+  theme = themes.mkCatppuccin { color = "Teal"; };
 
   fontOverrides = {
     terminal = nerdfonts.makeMono "Mononoki";
