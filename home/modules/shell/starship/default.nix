baebbc0ff18c05eaded1353c9a080284c857c799
Brandon Fulljames <bfullj@gmail.com>
2024-08-05 18:44:51 +0900

    Shorten starship AWS

--- home/modules/shell/starship/default.nix
+++ home/modules/shell/starship/default.nix
@@ -23,6 +23,9 @@ in {
       enable = true;
 
       settings = {
+        aws.format =
+          "[$symbol($profile )(\\($region\\) )(\\[$duration\\] )]($style)";
+
         # Don't show info about being in a container.
         # This fixes a weird thing where it says "Systemd" at the start of the prompt in WSL.
         # https://www.reddit.com/r/fishshell/comments/yhoi28/im_using_starship_prompt_in_wsl_and_it_keep/
