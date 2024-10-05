d4e4456740254df3dfa24caec0d62aeb4c639a6d
Brandon Fulljames <bfullj@gmail.com>
2024-10-05 11:24:24 +0900

    Add qemu to virtualization

--- system/modules/virtualization/default.nix
+++ system/modules/virtualization/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 with lib;
 let cfg = config.evertras.system.virtualization;
 in {
@@ -10,5 +10,14 @@ in {
     # https://nixos.wiki/wiki/Virt-manager
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
+
+    environment.systemPackages = with pkgs;
+      [
+        # https://wiki.nixos.org/wiki/QEMU
+        qemu
+      ];
+
+    # Enable other architectures for qemu
+    boot.binfmt.emulatedSystems = [ "aarch64-linux" "riscv64-linux" ];
   };
 }
