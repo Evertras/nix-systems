7a238fdaccb0d95740e4c75272734df0e86fd6e1
Brandon Fulljames <bfullj@gmail.com>
2024-11-16 16:39:05 +0900

    Add a quick nix flake init for dev shells

--- home/modules/shell/funcs/common.nix
+++ home/modules/shell/funcs/common.nix
@@ -79,6 +79,38 @@ in {
       '';
     };
 
+    nix-dev-init = {
+      body = ''
+        if [ -f flake.nix ]; then
+          log-fatal "flake.nix already exists"
+        fi
+
+        cat > flake.nix <<EOF
+        {
+          inputs = {
+            # TODO: Update if necessary
+            nixpkgs.url = "nixpkgs/nixos-24.05";
+            flake-utils.url = "github:numtide/flake-utils";
+          };
+
+          outputs = { self, nixpkgs, flake-utils }:
+            flake-utils.lib.eachDefaultSystem (system:
+              let
+                pkgs = import nixpkgs { inherit system; };
+              in
+              {
+                devShells.default = pkgs.mkShell {
+                  packages = with pkgs; [
+                    # Dev tools
+                    hello
+                  ];
+                };
+              });
+        }
+        EOF
+      '';
+    };
+
     replace-all.body = ''
       if [ $# -ne 4 ]; then
         echo "USAGE: change-all <dir> <file-filter> <old-regex> <replacement>"
