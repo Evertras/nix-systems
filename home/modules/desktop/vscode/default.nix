c91d9efa832877b0c9f01e0acaa55a9844b45a09
Brandon Fulljames <bfullj@gmail.com>
2025-10-07 05:01:43 -0400

    Add config for vscode

--- home/modules/desktop/vscode/default.nix
+++ home/modules/desktop/vscode/default.nix
@@ -1,9 +1,7 @@
 { config, everlib, lib, ... }:
 with everlib;
 with lib;
-let
-  cfg = config.evertras.home.desktop.vscode;
-  theme = config.evertras.themes.selected;
+let cfg = config.evertras.home.desktop.vscode;
 in {
   options.evertras.home.desktop.vscode = { enable = mkEnableOption "vscode"; };
 
@@ -11,7 +9,42 @@ in {
     programs.vscode = {
       enable = true;
 
-      profiles.default = { enableUpdateCheck = false; };
+      profiles.default = {
+        enableUpdateCheck = false;
+
+        # Extensions are handled by user manually, tried to do definitions
+        # here but A) it's annoying and B) it broke, so...
+
+        userSettings = {
+          "vim.insertModeKeyBindings" = [{
+            before = [ "j" "k" ];
+            after = [ "<Esc>" ];
+          }];
+          "editor.autoClosingBrackets" = "never";
+          "workbench.colorTheme" = "Catppuccin Macchiato";
+          "explorer.confirmDragAndDrop" = false;
+          "makefile.configureOnOpen" = true;
+          "explorer.confirmDelete" = false;
+          "github.copilot.nextEditSuggestions.enabled" = true;
+          "editor.fontFamily" =
+            "Hasklug Nerd Font Mono, 'Courier New', monospace";
+        };
+
+        keybindings = [
+          {
+            "key" = "ctrl+shift+alt+i";
+            "command" = "extension.vim_ctrl+i";
+            "when" =
+              "editorTextFocus && vim.active && vim.use<C-i> && !inDebugRepl";
+          }
+          {
+            "key" = "ctrl+i";
+            "command" = "-extension.vim_ctrl+i";
+            "when" =
+              "editorTextFocus && vim.active && vim.use<C-i> && !inDebugRepl";
+          }
+        ];
+      };
     };
   };
 }
