496c2f73a9881c251c09b76b4ade1a075f24a88b
Brandon Fulljames <bfullj@gmail.com>
2025-10-20 14:37:55 -0400

    Add VPN

--- system/modules/tiledb/default.nix
+++ system/modules/tiledb/default.nix
@@ -20,6 +20,9 @@ in {
 
         # Recent python, exact version unimportant
         python312
+
+        # VPN tooling
+        wireguard-tools
       ];
 
       # Development vars
@@ -29,10 +32,25 @@ in {
       in {
         CGO_CFLAGS = "-I${tiledb-home-dir}/include";
         CGO_LDFLAGS = "-L${tiledb-home-dir}/lib64";
-        LD_LIBRARY_PATH = "${tiledb-home-dir}/lib64:\${LD_LIBRARY_PATH}";
         TILEDB_HOME = tiledb-home-dir;
         TILEDB_PATH = tiledb-home-dir;
         TILEDB_DEV_DATA = "${tiledb-home-dir}/devdata";
+      };
+    };
+
+    networking.wg-quick.interfaces = {
+      tiledb = {
+        autostart = false;
+        address = [ "10.100.0.50" ];
+        dns = [ "10.20.0.2" ];
+        privateKeyFile = "/home/evertras/.config/wireguard/privatekey";
+
+        peers = [{
+          publicKey = "7+yFrmQ/+R4zOiIHJnHtlq9cZBoz67GRVNeTzWj0Rlw=";
+          allowedIPs = [ "10.0.0.0/8" ];
+          endpoint = "wg-prod.tiledb.io:51820";
+          persistentKeepalive = 15;
+        }];
       };
     };
 
