9252e2b1fe53c21622681ab657c9cc2d2d581848
Brandon Fulljames <bfullj@gmail.com>
2025-10-20 17:02:54 -0400

    More advanced tdb shell

--- home/modules/tiledb/devshell/shell.nix
+++ home/modules/tiledb/devshell/shell.nix
@@ -1,11 +1,23 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (python3.withPackages (ps: with ps; [ numpy ]))
-    python3Packages.venvShellHook
+    #(python3.withPackages (ps: with ps; [ setuptools wheel pip ]))
+    # Don't manage any packages here for now, all in venv
+    python3
+
+    gcc
+    stdenv.cc.cc.lib
     zstd
     pkg-config
   ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$TILEDB_HOME/lib64"
+
+    source ~/.evertras/venv/tiledb/bin/activate
+
+    echo "TileDB shell activated"
+  '';
 
   venvDir = "/home/evertras/.evertras/venv/tiledb";
 }
