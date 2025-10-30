47432cda25e205605e6243756b5fd1cb90b5b1ba
Brandon Fulljames <bfullj@gmail.com>
2025-10-30 18:38:13 -0400

    Minor cleanup

--- system/machines/delltop/configuration.nix
+++ system/machines/delltop/configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }: {
+{ config, pkgs, ... }: {
   imports = [
     ../../modules
 
@@ -53,8 +53,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
-  #console = { keyMap = "jp106"; };
 
   evertras.system.virtualization.enable = true;
 
