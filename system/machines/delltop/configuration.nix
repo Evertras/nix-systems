a19d8f8438421b772783b599755fe9da7112f89a
Brandon Fulljames <bfullj@gmail.com>
2025-11-10 14:25:54 -0500

    Use docker module for delltop

--- system/machines/delltop/configuration.nix
+++ system/machines/delltop/configuration.nix
@@ -56,12 +56,7 @@
 
   evertras.system.virtualization.enable = true;
 
-  virtualisation.docker.rootless = {
-    enable = true;
-    setSocketVariable = true;
-
-    daemon.settings = { dns = [ "1.1.1.1" "8.8.8.8" ]; };
-  };
+  evertras.system.docker.enable = true;
 
   evertras.dev.tiledb.enable = true;
 
