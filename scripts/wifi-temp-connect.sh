441203a012d8a3e916d030f575e166cc5515752b
Brandon Fulljames <bfullj@gmail.com>
2025-11-12 10:16:22 -0500

    Better wifi temp connect

--- scripts/wifi-temp-connect.sh
+++ scripts/wifi-temp-connect.sh
@@ -2,7 +2,6 @@
 
 # TODO: configurable with default
 interface=wlo1
-config_file=/tmp/evertras-tmp-wifi.conf
 
 set -e
 
@@ -10,7 +9,5 @@ read -r -p "SSID: " ssid
 read -r -s -p "Password: " password
 echo ""
 
-wpa_passphrase "$ssid" "$password" | grep -v "#psk" > ${config_file}
-
 echo "Starting wpa_supplicant for SSID $ssid"
-sudo wpa_supplicant -c /tmp/evertras-tmp-wifi.conf -B -i "${interface}"
+sudo wpa_supplicant -B -i "${interface}" <(wpa_passphrase "$ssid" "$password")
