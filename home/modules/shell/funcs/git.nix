de4b270095ce3b2bce0295af290e9e161344d7aa
Brandon Fulljames <bfullj@gmail.com>
2026-03-27 13:58:48 -0400

    Fix gadd null bytes

--- home/modules/shell/funcs/git.nix
+++ home/modules/shell/funcs/git.nix
@@ -21,11 +21,7 @@
 
     gadd.body = ''
       cd "$(git rev-parse --show-toplevel)"
-      to_add=$(git status --porcelain | awk '/^.M/ || /^\?\?/ {printf "%s\0", $2}' | fzf --scheme=path -i --tiebreak=end --preview 'git diff --color {+1}' --read0 --print0)
-      if [ -n "$to_add" ]; then
-        git add "$to_add"
-        echo "Added $to_add"
-      fi
+      git status --porcelain | awk '/^.M/ || /^\?\?/ {printf "%s\0", $2}' | fzf --scheme=path -i --tiebreak=end --preview 'git diff --color {+1}' --read0 --print0 | xargs -0 -r git add
     '';
   };
 }
