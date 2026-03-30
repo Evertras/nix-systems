9d3b1351e744718799634ae3f88ec503a3670832
Brandon Fulljames <bfullj@gmail.com>
2026-03-30 15:22:53 -0400

    Add nftables to system default

--- system/modules/base/default.nix
+++ system/modules/base/default.nix
@@ -1,5 +1,11 @@
 # The non-negotiables that every system must have defined
-{ ... }: {
+{ pkgs, ... }: {
   # Never touch this, we need this for the whole setup to work
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  environment.systemPackages = with pkgs;
+    [
+      # By default iptables uses nftables, but we want to have the CLI command too
+      nftables
+    ];
 }
