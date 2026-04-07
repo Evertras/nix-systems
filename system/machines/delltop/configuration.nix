b0d8e72b216e99f8189c229a976acc8a8934544c
Brandon Fulljames <bfullj@gmail.com>
2026-04-07 14:41:13 -0400

    Trust myself

--- system/machines/delltop/configuration.nix
+++ system/machines/delltop/configuration.nix
@@ -204,6 +204,9 @@
     enableSSHSupport = true;
   };
 
+  # Trust myself
+  nix.settings.trusted-users = [ "evertras" ];
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
