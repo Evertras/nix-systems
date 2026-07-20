d4df9072bfd5b836f01ec1cc46d6c51a81478542
Brandon Fulljames <bfullj@gmail.com>
2026-07-20 10:27:06 -0400

    Auto gc

--- system/modules/base/default.nix
+++ system/modules/base/default.nix
@@ -1,11 +1,19 @@
 # The non-negotiables that every system must have defined
 { pkgs, ... }: {
   # Never touch this, we need this for the whole setup to work
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-  environment.systemPackages = with pkgs;
-    [
-      # By default iptables uses nftables, but we want to have the CLI command too
-      nftables
-    ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  environment.systemPackages = with pkgs; [
+    # By default iptables uses nftables, but we want to have the CLI command too
+    nftables
+  ];
 }
